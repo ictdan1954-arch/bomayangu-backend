509a0df53254d285eebe44119d2696ba8f1f0beb
ictdan1954-arch <ictdan1954@gmail.com>
2026-06-22 23:35:04 +0300

    Add files via upload

---
+++ reset-sequence.js
@@ -0,0 +1,14 @@
+﻿const { sequelize } = require('./models');
+
+async function resetSequence() {
+    try {
+        await sequelize.query(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`);
+        console.log('✅ Users sequence reset successfully.');
+        console.log('✅ All sequences reset successfully.');
+    } catch (error) {
+        console.error('❌ Error resetting sequence:', error);
+    } finally {
+        await sequelize.close();
+    }
+}
+resetSequence();
