8e90d58404ceec85491580bf8f56fe0b93eb8905
ictdan1954-arch <ictdan1954@gmail.com>
2026-06-22 19:21:05 +0300

    Update callback.routes.js

--- routes/callback.routes.js
+++ routes/callback.routes.js
@@ -1,8 +1,14 @@
-﻿const express = require('express');
+const express = require('express');
 const router = express.Router();
 const callbackController = require('../controllers/callback.controller');
 
-router.post('/mpesa', callbackController.mpesaCallback);
-router.post('/paybill', callbackController.paybillCallback);
+// ---------- PAYHERO CALLBACK ----------
+// PayHero sends payment confirmation to this endpoint after STK Push
+router.post('/payhero', callbackController.payheroCallback);
+
+// ---------- (REMOVED) M-PESA AND PAYBILL CALLBACKS ----------
+// These are no longer used – we've migrated to PayHero.
+// router.post('/mpesa', callbackController.mpesaCallback);
+// router.post('/paybill', callbackController.paybillCallback);
 
 module.exports = router;
